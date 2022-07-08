89e717e11fba11d526bae90bd24058176cbc30fa
Tuneer Roy <tuneer.roy12@gmail.com>
2022-07-07 21:42:42 -0500

    feat: submit endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,6 +39,40 @@ app.get('/api/supervisors', (_req, res) => {
     });
 });
 
+const validatePost = (req, res, next) => {
+    // check for missing required elements
+    const missing = ['firstName', 'lastName', 'supervisor'].filter(key => !(key in req.body));
+    if (missing.length > 0) 
+    return res.status(400).send(`Missing required fields: ${missing.join(', ')}`);
+
+    // validate names
+    if (/^[a-zA-Z]+$/.test(req.body.firstName) || /^[a-zA-Z]+$/.test(req.body.lastName) || /^[a-zA-Z]+$/.test(req.body.supervisor)) 
+    return res.status(400).send('Names must only contain letters.');
+    
+    // validate email if exists (uses RFC 2822, directly taken from https://stackoverflow.com/a/1373724)
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    if ('email' in req.body && !req.body.email.match(emailRegex)) 
+    return res.status(400).send('Email is invalid.');
+
+    // validate phone number if exists (directly taken from https://stackoverflow.com/q/4338267)
+    const numberRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    if ('phoneNumber' in req.body && !req.body.phoneNumber.match(numberRegex)) 
+    return res.status(400).send('Phone number is invalid.');
+
+    next();
+}
+
+app.post('/api/submit', validatePost, (req, res) => {
+    console.log({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        ... 'email' in req.body && {email: req.body.email},
+        ... 'phoneNumber' in req.body && {phoneNumber: req.body.phoneNumber},
+        supervisor: req.body.supervisor
+    })
+    res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}. Ctrl-C to stop server.`);
 });
